f26a68074ccdb33065d9445a1f89292afc6d436d
fjod <fourjustoneday@yandex.ru>
2021-02-16 11:50:00 +0300

    moved ToyFactory.Data dbContext from MSSQL to SqLite; added sample database; applied migrations to it

---
+++ Playground/Reinforced.Samples.ToyFactory.Data/Migrations/20210216084523_initial.cs
@@ -0,0 +1,219 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Reinforced.Samples.ToyFactory.Data.Migrations
+{
+    public partial class initial : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "MeasurementUnits",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    ShortName = table.Column<string>(type: "TEXT", nullable: true),
+                    Name = table.Column<string>(type: "TEXT", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_MeasurementUnits", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "ResourceSupplies",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Name = table.Column<string>(type: "TEXT", nullable: true),
+                    CreationDate = table.Column<DateTime>(type: "TEXT", nullable: false),
+                    Status = table.Column<int>(type: "INTEGER", nullable: false),
+                    ItemsCount = table.Column<int>(type: "INTEGER", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ResourceSupplies", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "ToyTypes",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Name = table.Column<string>(type: "TEXT", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ToyTypes", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Resources",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Name = table.Column<string>(type: "TEXT", nullable: true),
+                    StockQuantity = table.Column<double>(type: "REAL", nullable: false),
+                    MeasurementUnitId = table.Column<int>(type: "INTEGER", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Resources", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Resources_MeasurementUnits_MeasurementUnitId",
+                        column: x => x.MeasurementUnitId,
+                        principalTable: "MeasurementUnits",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "ResourceSupplyStatusHistoryItems",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Previous = table.Column<int>(type: "INTEGER", nullable: true),
+                    Next = table.Column<int>(type: "INTEGER", nullable: false),
+                    Comment = table.Column<string>(type: "TEXT", nullable: true),
+                    CreatedDate = table.Column<DateTime>(type: "TEXT", nullable: false),
+                    ResourceSupplyId = table.Column<int>(type: "INTEGER", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ResourceSupplyStatusHistoryItems", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_ResourceSupplyStatusHistoryItems_ResourceSupplies_ResourceSupplyId",
+                        column: x => x.ResourceSupplyId,
+                        principalTable: "ResourceSupplies",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Blueprints",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    ToyTypeId = table.Column<int>(type: "INTEGER", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Blueprints", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Blueprints_ToyTypes_ToyTypeId",
+                        column: x => x.ToyTypeId,
+                        principalTable: "ToyTypes",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "ResourceSupplyItems",
+                columns: table => new
+                {
+                    ResourceId = table.Column<int>(type: "INTEGER", nullable: false),
+                    ResourceSupplyId = table.Column<int>(type: "INTEGER", nullable: false),
+                    Quantity = table.Column<double>(type: "REAL", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ResourceSupplyItems", x => new { x.ResourceSupplyId, x.ResourceId });
+                    table.ForeignKey(
+                        name: "FK_ResourceSupplyItems_Resources_ResourceId",
+                        column: x => x.ResourceId,
+                        principalTable: "Resources",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_ResourceSupplyItems_ResourceSupplies_ResourceSupplyId",
+                        column: x => x.ResourceSupplyId,
+                        principalTable: "ResourceSupplies",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "BlueprintResources",
+                columns: table => new
+                {
+                    ResourceId = table.Column<int>(type: "INTEGER", nullable: false),
+                    BlueprintId = table.Column<int>(type: "INTEGER", nullable: false),
+                    Quantity = table.Column<double>(type: "REAL", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_BlueprintResources", x => new { x.BlueprintId, x.ResourceId });
+                    table.ForeignKey(
+                        name: "FK_BlueprintResources_Blueprints_BlueprintId",
+                        column: x => x.BlueprintId,
+                        principalTable: "Blueprints",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_BlueprintResources_Resources_ResourceId",
+                        column: x => x.ResourceId,
+                        principalTable: "Resources",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_BlueprintResources_ResourceId",
+                table: "BlueprintResources",
+                column: "ResourceId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Blueprints_ToyTypeId",
+                table: "Blueprints",
+                column: "ToyTypeId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Resources_MeasurementUnitId",
+                table: "Resources",
+                column: "MeasurementUnitId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_ResourceSupplyItems_ResourceId",
+                table: "ResourceSupplyItems",
+                column: "ResourceId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_ResourceSupplyStatusHistoryItems_ResourceSupplyId",
+                table: "ResourceSupplyStatusHistoryItems",
+                column: "ResourceSupplyId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "BlueprintResources");
+
+            migrationBuilder.DropTable(
+                name: "ResourceSupplyItems");
+
+            migrationBuilder.DropTable(
+                name: "ResourceSupplyStatusHistoryItems");
+
+            migrationBuilder.DropTable(
+                name: "Blueprints");
+
+            migrationBuilder.DropTable(
+                name: "Resources");
+
+            migrationBuilder.DropTable(
+                name: "ResourceSupplies");
+
+            migrationBuilder.DropTable(
+                name: "ToyTypes");
+
+            migrationBuilder.DropTable(
+                name: "MeasurementUnits");
+        }
+    }
+}
--- Playground/Reinforced.Samples.ToyFactory.Data/ToyFactoryDbContext.cs
+++ Playground/Reinforced.Samples.ToyFactory.Data/ToyFactoryDbContext.cs
@@ -1,35 +1,21 @@
 ﻿using Microsoft.EntityFrameworkCore;
 using Reinforced.Samples.ToyFactory.Logic.Entities;
-using Reinforced.Samples.ToyFactory.Logic.Warehouse.Entities.Suppliement;
 
 namespace Reinforced.Samples.ToyFactory.Data
 {
     public partial class ToyFactoryDbContext : DbContext
     {
-        private const string DefaultConnection =
-            @"Data Source=.\MSSQL2017;Initial Catalog=ToyFactory;Integrated Security=True";
-
-        private readonly string _connectionString;
-        public ToyFactoryDbContext(string connectionString)
-        {
-            _connectionString = connectionString;
-        }
-        public ToyFactoryDbContext() : this(DefaultConnection) { }
+        private const string DefaultConnection = "Data Source=TectureSample.db;Cache=Shared";
 
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
-            optionsBuilder.UseSqlServer(_connectionString);
+            optionsBuilder.UseSqlite(DefaultConnection);
         }
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
-            modelBuilder.Entity<BlueprintResources>().HasKey(x => new { x.BlueprintId, x.ResourceId });
+            modelBuilder.Entity<BlueprintResources>().HasKey(x => new {x.BlueprintId, x.ResourceId});
             OnWarehouseModelCreating(modelBuilder);
         }
-        public DbSet<ToyType> ToyTypes { get; set; }
-        public DbSet<Blueprint> Blueprints { get; set; }
-        public DbSet<BlueprintResources> BlueprintResources { get; set; }
-
-        
     }
 }
